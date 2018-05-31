51d5d4c175fd4551d42d3ee51135ae594edffbed
zzzadruga <zzzadruga@gmail.com>
2018-05-31 16:15:49 +0300

    create services for math operations: add, subtract, multiply, divide

---
+++ src/main/java/ru/zzzadruga/services/mathOperations/DivideService.java
@@ -0,0 +1,42 @@
+package ru.zzzadruga.services.mathOperations;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteAtomicSequence;
+import org.apache.ignite.resources.IgniteInstanceResource;
+import org.apache.ignite.services.ServiceContext;
+import ru.zzzadruga.services.mathOperations.common.MathOperationService;
+
+import java.math.BigDecimal;
+
+public class DivideService implements MathOperationService {
+    @IgniteInstanceResource
+    private Ignite ignite;
+    public static final String SERVICE_NAME = "DivideService";
+    private IgniteAtomicSequence sequence;
+
+    @Override
+    public BigDecimal calculate(BigDecimal operand1, BigDecimal operand2) {
+        sequence.getAndIncrement();
+        return operand1.divide(operand2);
+    }
+
+    @Override
+    public void cancel(ServiceContext ctx) {
+
+    }
+
+    @Override
+    public void init(ServiceContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void execute(ServiceContext ctx) throws Exception {
+        sequence = ignite.atomicSequence("divideCounter", 0, true);
+    }
+
+    @Override
+    public long getCount() {
+        return sequence.get();
+    }
+}
---
+++ src/main/java/ru/zzzadruga/services/mathOperations/MultiplyService.java
@@ -0,0 +1,42 @@
+package ru.zzzadruga.services.mathOperations;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteAtomicSequence;
+import org.apache.ignite.resources.IgniteInstanceResource;
+import org.apache.ignite.services.ServiceContext;
+import ru.zzzadruga.services.mathOperations.common.MathOperationService;
+
+import java.math.BigDecimal;
+
+public class MultiplyService implements MathOperationService {
+    @IgniteInstanceResource
+    private Ignite ignite;
+    public static final String SERVICE_NAME = "MultiplyService";
+    private IgniteAtomicSequence sequence;
+
+    @Override
+    public BigDecimal calculate(BigDecimal operand1, BigDecimal operand2) {
+        sequence.getAndIncrement();
+        return operand1.multiply(operand2);
+    }
+
+    @Override
+    public void cancel(ServiceContext ctx) {
+
+    }
+
+    @Override
+    public void init(ServiceContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void execute(ServiceContext ctx) throws Exception {
+        sequence = ignite.atomicSequence("multiplyCounter", 0, true);
+    }
+
+    @Override
+    public long getCount() {
+        return sequence.get();
+    }
+}
