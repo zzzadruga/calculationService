7ed5453655fdcf5c28be6e3fc3a041327bb5aa37
zzzadruga <zzzadruga@gmail.com>
2018-06-07 11:43:32 +0300

    refactor balancer

--- src/main/java/ru/zzzadruga/common/filters/Balancer.java
+++ src/main/java/ru/zzzadruga/common/filters/Balancer.java
@@ -1,11 +1,14 @@
 package ru.zzzadruga.common.filters;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.cluster.ClusterNode;
 import ru.zzzadruga.services.calculation.common.CalculationService;
+import ru.zzzadruga.services.mathOperations.common.MathOperationService;
 
 public class Balancer {
     public static boolean getResult(Set<ClusterNode> usedNodes, ClusterNode node, Ignite ignite, String serviceName) {
@@ -22,18 +25,18 @@ public class Balancer {
                 long amountOperations = calculationServiceProxy.getAmount();
                 System.out.println(amountOperations);
                 if (amountOperations != 0) {
-                    long addCount = calculationServiceProxy.getMap().get(serviceName).getCount();
-                    long amountNodes = allNodes.size();
-                    long addNodes = usedNodes.size();
-                    long operationsPerc = (long)(((float)addCount / (float)amountOperations) * 100);
-                    long nodesPerc = (long)(((float)addNodes / (float)amountNodes) * 100);
-                    if (operationsPerc > nodesPerc) {
+                    long max = calculationServiceProxy.getMap().entrySet().stream()
+                        .max(Map.Entry.comparingByValue(Comparator.comparingLong(MathOperationService::getCount)))
+                        .get().getValue().getCount();
+                    int servicesPerNode = (int)Math.ceil(amountOperations / (double)max);
+                    long operationCount = calculationServiceProxy.getMap().get(serviceName).getCount();
+                    int amountNodes = allNodes.size();
+                    int operationNodes = usedNodes.size();
+                    int needNodes = Math.round(operationCount/(float)max * amountNodes);
+                    if ((operationNodes < needNodes)){
                         usedNodes.add(node);
                         return true;
                     }
-                }
-                else {
-                    return false;
                 }
             }
         }
